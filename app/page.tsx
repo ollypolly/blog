fbad71d267df2862aef3725f0745d96fda3fb495
Olly Pethick <ollypethick@gmail.com>
2025-09-04 18:29:46 +0100

    Add more test posts

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
           Latest Posts
         </h1>
         <p className="text-gray-600 dark:text-gray-400">
-          Thoughts, ideas, and stories from my journey
+          Here's some things I decided to do
         </p>
       </section>
 
