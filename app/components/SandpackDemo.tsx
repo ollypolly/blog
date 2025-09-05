e5f6a4915718a6b513af417d23ca5d2f7f3ffca7
Olly Pethick <ollypethick@gmail.com>
2025-09-05 01:43:02 +0100

    Make sandpack demo wider

--- app/components/SandpackDemo.tsx
+++ app/components/SandpackDemo.tsx
@@ -37,16 +37,18 @@ export default function SandpackDemo({
   }
 
   return (
-    <Sandpack
-      template={template}
-      files={files}
-      options={{
-        showNavigator,
-        showTabs,
-        showLineNumbers,
-        editorHeight,
-      }}
-      theme={resolvedTheme as SandpackThemeProp}
-    />
+    <div className="w-[min(100vw-3rem,60rem)] max-w-none mx-auto relative left-1/2 -translate-x-1/2 sm:w-[min(100vw-4rem,60rem)] lg:w-[min(100vw-5rem,60rem)]">
+      <Sandpack
+        template={template}
+        files={files}
+        options={{
+          showNavigator,
+          showTabs,
+          showLineNumbers,
+          editorHeight,
+        }}
+        theme={resolvedTheme as SandpackThemeProp}
+      />
+    </div>
   );
 }
