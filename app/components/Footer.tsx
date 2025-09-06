99a4c10362b426eaac030a35e9eafb30e7e91209
Olly Pethick <ollypethick@gmail.com>
2025-09-06 11:55:11 +0100

    Update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,6 @@
 export const Footer = () => {
   return (
     <footer className="text-center py-8 text-gray-500 dark:text-gray-400 text-sm">
-      <p>&copy; 2025 All rights reserved.</p>
       <p className="mt-1">
         Made with <span className="text-gray-500 dark:text-gray-400">♥</span> by{' '}
         <a
@@ -13,6 +12,7 @@ export const Footer = () => {
           @ollypolly
         </a>
       </p>
+      <p>&copy; 2025</p>
     </footer>
   );
 };
